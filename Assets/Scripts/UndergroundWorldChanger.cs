ddc7ea0b217b5b294bc6303146f5c30c8ec67e9f
Michal <malpa.w.klapkach@gmail.com>
2024-01-27 01:59:36 +0100

    Add underground world changer

---
+++ Assets/Scripts/UndergroundWorldChanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UndergroundWorldChanger : MonoBehaviour
+{
+    private const float undergroundAngle = -180;
+    private const float overgroundAngle = 0;
+
+    [SerializeField]
+    private bool isUnderground;
+    public bool IsUnderground
+    {
+        get => isUnderground;
+        set
+        {
+            StopAllCoroutines();
+            isUnderground = value;
+            StartCoroutine(TransitionWorldCo());
+        }
+    }
+
+    [SerializeField]
+    private float rotationSpeed = 300;
+
+    private IEnumerator TransitionWorldCo()
+    {
+        float targetAngle = isUnderground ? undergroundAngle : overgroundAngle;
+        float startAngle = transform.localEulerAngles.z;
+        float progress = 0;
+        float progressSpeed = rotationSpeed / Mathf.Abs(startAngle - targetAngle);
+        while (progress < 1)
+        {
+            progress += Time.deltaTime * progressSpeed;
+            float angle = Mathf.Lerp(startAngle, targetAngle, progress);
+            transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            yield return null;
+        }
+        transform.localRotation = Quaternion.AngleAxis(targetAngle, Vector3.forward);
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying) 
+            IsUnderground = IsUnderground;
+    }
+}
